1efba15ca84c8911733c016d149010b64be7f739
Junpyo Lim <djfl112@naver.com>
2023-03-02 18:27:50 +0900

    first commit

---
+++ Lecture23.py
@@ -0,0 +1,15 @@
+# 홀수 짝수 구분
+number = int(input("정수 입력: "))
+if number % 2 == 0:
+    print("짝수입니다")
+if number % 2 == 1:
+    print("홀수입니다")
+
+# else 구문
+if 조건문:
+    문장
+else:
+    조건문 값이 False일 때 실행
+
+# elif 구문
+if 와 else 사이에서 씀
