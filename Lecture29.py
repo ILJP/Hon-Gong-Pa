da75bd01dd7ed378417c00f46e35c510dba19567
Junpyo Lim <djfl112@naver.com>
2023-03-03 17:06:04 +0900

    first commit

---
+++ Lecture29.py
@@ -0,0 +1,28 @@
+# 항등원: 임의의 원소에 특정 연산을 했을 때 재귀시키는 원소
++의 항등원: 0
+*의 항등원: 1
+
+# 총합과 총곱
+-총합: 모두 더한다
+-총곱: 모두 곱한다
+
+# for 반복문
+for 반복변수 in 리스트:
+    복합구문
+
+ex)
+a = [1, 2, 3, 4, 5]
+for i in a:
+    pring(i)
+
+# 총합
+sum = 0 #항등원
+for i in in a:
+    sum = sum + i
+print(sum)
+
+# 총곱
+product = 1 #항등원
+for i in a:
+    product = product * i
+print(product)
